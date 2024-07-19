abd9d7fcbc2690ac28cdf60de15fab324137c766
Daniel Ye <48700147+daniel-yili-ye@users.noreply.github.com>
2024-07-19 01:26:31 -0400

    Revert "wip (#3)" (#5)
    This reverts commit 8a467add4af5bb8718eced234adeba9fc4a9af89.

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,54 +1,25 @@
-import { useState, useEffect } from "react";
 import Image from "next/image";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
+  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
-  TableFooter,
 } from "@/components/ui/table";
 import { FormData } from "@/lib/formSchema";
-import { splitBill, BillAllocation } from "@/utils/billSplitter";
-import { Loader2 } from "lucide-react";
-import { Share2Icon } from "@radix-ui/react-icons";
 
 export default function Summary({ formData }: { formData: FormData }) {
-  const [allocation, setAllocation] = useState<BillAllocation | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const calculateAllocation = async () => {
-      try {
-        const result = await splitBill(formData);
-        setAllocation(result);
-      } catch (error) {
-        console.error("Error calculating bill split:", error);
-        // Handle error (e.g., show error message to user)
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    calculateAllocation();
-  }, [formData]);
-
-  const totalBill = allocation
-    ? allocation.people.reduce((sum, person) => sum + person.subtotal, 0)
-    : 0;
-
   return (
     <Card>
       <CardHeader>
         <div className="flex justify-between items-center">
           <CardTitle>{formData.stepOne.mealName}</CardTitle>
-          <Button>
-            Share&nbsp;
-            <Share2Icon />
-          </Button>
+          {/* <Button variant="outline">Edit in Google Sheets</Button> */}
+          <Button>Share</Button>
         </div>
       </CardHeader>
       <CardContent className="space-y-4">
@@ -61,43 +32,59 @@ export default function Summary({ formData }: { formData: FormData }) {
             objectFit="contain"
           />
         )}
+        <Card>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Item</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {formData.stepThree.foodItems.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">{item.item}</TableCell>
+                  <TableCell className="text-right">${item.price}</TableCell>
+                </TableRow>
+              ))}
+              <TableRow>
+                <TableCell className="font-medium">Tax</TableCell>
+                <TableCell className="text-right">
+                  ${formData.stepThree.tax}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium">Tip</TableCell>
+                <TableCell className="text-right">
+                  ${formData.stepThree.tip}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium">Discount</TableCell>
+                <TableCell className="text-right">
+                  $({formData.stepThree.discount})
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Card>
 
-        {isLoading ? (
-          <div className="flex justify-center items-center h-40">
-            <Loader2 className="h-8 w-8 animate-spin" />
-          </div>
-        ) : allocation ? (
-          <Card>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Name</TableHead>
-                  <TableHead className="text-right">Subtotal</TableHead>
+        <Card>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Participants</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {formData.stepFour.map((person, index) => (
+                <TableRow key={index}>
+                  <TableCell>{person.name}</TableCell>
                 </TableRow>
-              </TableHeader>
-              <TableBody>
-                {allocation.people.map((person, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{person.name}</TableCell>
-                    <TableCell className="text-right">
-                      ${(person.subtotal / 100).toFixed(2)}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-              <TableFooter>
-                <TableRow>
-                  <TableCell>Total</TableCell>
-                  <TableCell className="text-right">
-                    ${(totalBill / 100).toFixed(2)}
-                  </TableCell>
-                </TableRow>
-              </TableFooter>
-            </Table>
-          </Card>
-        ) : (
-          <p>Error calculating bill split. Please try again.</p>
-        )}
+              ))}
+            </TableBody>
+          </Table>
+        </Card>
       </CardContent>
     </Card>
   );
