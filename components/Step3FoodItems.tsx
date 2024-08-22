7972e3a2f5dee03f7a4cd7fb22d9baf34c0f6909
Daniel Ye <48700147+daniel-yili-ye@users.noreply.github.com>
2024-08-22 00:06:29 -0400

    Additional fields (#7)
    * additional fields
    
    * additional updates to include new fields

--- components/Step3FoodItems.tsx
+++ components/Step3FoodItems.tsx
@@ -10,8 +10,14 @@ import {
 } from "./ui/form";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { Calendar } from "./ui/calendar";
+import { Popover, PopoverTrigger, PopoverContent } from "./ui/popover";
 import { FormData } from "../lib/formSchema";
 import { PlusIcon } from "@radix-ui/react-icons";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
+import { Separator } from "./ui/separator";
 
 export default function Step3FoodItems() {
   const { control } = useFormContext<FormData>();
@@ -46,8 +52,64 @@ export default function Step3FoodItems() {
   };
 
   return (
-    <div>
-      <div className="mb-4">
+    <div className="space-y-4">
+      <div className="space-y-4">
+        <FormField
+          control={control}
+          name="stepThree.restaurantName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Restaurant Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Restaurant name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={control}
+          name="stepThree.date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Receipt Date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+      <div>
         <div className="space-y-2">
           <FormLabel>Items</FormLabel>
           {fieldsFoodItems.map((field, index) => (
