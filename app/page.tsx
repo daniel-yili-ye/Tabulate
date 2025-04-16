1f308079616f9c636b7f07e31f7bf33331eb4f83
daniel-yili-ye <daniel.ye@live.com>
2025-04-15 21:40:36 -0400

    fix bottom margin

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import MultiStepForm from "@/components/custom/MultiStepForm";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center mb-6">
-        <MultiStepForm />
-      </div>
+      <MultiStepForm />
     </>
   );
 }
