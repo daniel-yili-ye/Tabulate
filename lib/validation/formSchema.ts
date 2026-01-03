89acf92bd7848d91ca2cc143f44e0da54634f0a8
Daniel Ye <48700147+daniel-yili-ye@users.noreply.github.com>
2026-01-02 19:02:34 -0500

    migrated to new shadcn primatives (#23)
    * migrated to new shadcn primatives
    
    * fix build error bugs
    
    * fix dev deps

--- lib/validation/formSchema.ts
+++ lib/validation/formSchema.ts
@@ -10,9 +10,12 @@ const wizard1Schema = z.object({
   receiptImageURL: z.any().optional(),
   image: z
     .any()
-    .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
     .refine(
-      (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+      (file) => !file || file.size <= MAX_FILE_SIZE,
+      `Max image size is 5MB.`
+    )
+    .refine(
+      (file) => !file || ACCEPTED_IMAGE_TYPES.includes(file.type),
       "Only .jpg, .jpeg, and .png formats are supported."
     )
     .optional(),
